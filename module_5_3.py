28763b24983248821688fce9948b2f48a54bc71b
Dmitry <kadzzi@gmail.com>
2024-07-19 16:55:11 +0300

    module_5_3.py PEP8

--- module_5_3.py
+++ module_5_3.py
@@ -25,23 +25,18 @@ class House:
     def __le__(self, other):
         return self.number_of_floors <= other.number_of_floors
 
-
     def __gt__(self, other):
         return self.number_of_floors > other.number_of_floors
-
 
     def __ge__(self, other):
         return self.number_of_floors >= other.number_of_floors
 
-
     def __ne__(self, other):
-         return self.number_of_floors != other.number_of_floors
-
+        return self.number_of_floors != other.number_of_floors
 
     def __add__(self, other):
         self.number_of_floors = self.number_of_floors + other
         return self
-
 
     def __iadd__(self, other):
         return self.__add__(other)
@@ -49,26 +44,27 @@ class House:
     def __radd__(self, other):
         return self.__add__(other)
 
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10 # __add__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
